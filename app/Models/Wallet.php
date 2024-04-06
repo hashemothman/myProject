2a85b10fd075e00d783ac6d23209d0bf453639a5
YousefSaleh1 <ys9882111@gmail.com>
2024-04-06 12:13:19 +0300

    Refactor model classes to include authentication checks in the creating event

--- app/Models/Wallet.php
+++ app/Models/Wallet.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Auth;
 
 class Wallet extends Model
 {
@@ -17,6 +18,15 @@ class Wallet extends Model
         'max_amount_id',
         'amount',
     ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        self::creating(function ($wallet) {
+            $wallet->user_id = Auth::user()->id;
+        });
+    }
 
     public function user(): BelongsTo
     {
