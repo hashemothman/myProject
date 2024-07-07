d63ea297f902e69ea0e73b26e8e56b58302666a4
hashim othman <hashim993.hel993@gmail.com>
2024-07-07 09:59:35 +0300

    new edit

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -48,19 +48,28 @@ class AuthController extends Controller
         }
 
         $user = Auth::user();
-        $data = new UserResource($user);
-        return $this->apiResponse($data, $token, 'User Login successfully', 200);
+        $data = [new UserResource($user), $token];
+        return $this->apiResponse($data, 'User Login successfully', 200);
     }
 
     public function register(UserRegisterRequest $request)
     {
-        $user = User::create([
-            'email'         => $request->email,
-            'mobile_number' => $request->mobile_number,
-            'password'      => Hash::make($request->password),
-        ]);
+        if($request->has('mobile_number')){
+            $user = User::create([
+                'mobile_number' => $request->mobile_number,
+                'password'      => Hash::make($request->password),
+            ]);
+        }
+        if($request->has('email')){
+            $user = User::create([
+                'email'         => $request->email,
+                'password'      => Hash::make($request->password),
+            ]);
+        }
 
-        $data = new UserResource($user);
+        $token = Auth::login($user);
+
+        $data = ['user'=>new UserResource($user),'token'=>$token];
         return $this->customeResponse($data, 'User Register successfully', 201);
     }
 
--- app/Http/Controllers/API/TransactionController.php
+++ app/Http/Controllers/API/TransactionController.php
@@ -10,7 +10,7 @@ use App\Http\Traits\ApiResponseTrait;
 use App\Http\Requests\TransactionRequest;
 use App\Http\Resources\TransactionResource;
 use App\Http\Requests\UpdateTransactionRequest;
-use App\Models\Transaction;
+
 
 class TransactionController extends Controller
 {
