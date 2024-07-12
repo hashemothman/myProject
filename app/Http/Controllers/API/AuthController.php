e19c34b92af2d8782110e4b6bdca4f3484b6b08c
hashim othman <hashim993.hel993@gmail.com>
2024-07-12 20:44:03 +0300

    complate

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -31,7 +31,7 @@ class AuthController extends Controller
     public function __construct()
     {
         # By default we are using here auth:api middleware
-        $this->middleware('auth:api', ['except' => ['login', 'register']]);
+        $this->middleware('auth:api', ['except' => ['login', 'register','registerPhone']]);
     }
 
     public function login(UserLoginRequest $request)
