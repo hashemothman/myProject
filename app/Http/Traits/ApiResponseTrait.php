0c9060a780c9f1c0065241f2ff782a2c9b6f3c47
Ayham Ibrahim <146968216+Ayham-Ibrahim@users.noreply.github.com>
2024-08-27 00:55:25 +0300

    Update ApiResponseTrait.php

--- app/Http/Traits/ApiResponseTrait.php
+++ app/Http/Traits/ApiResponseTrait.php
@@ -27,4 +27,24 @@ trait ApiResponseTrait
 
         return response()->json($array, $status);
     }
+
+        //this function do the same functionality of the previous paginated function , but accepts resources as input
+    public function resourcePaginated($data,$message = 'Operation Success',$status = 200){
+        $paginator = $data->resource;
+        $resourceData = $data->items();
+
+        $array = [
+            'status' => 'success',
+            'message'=>trans($message),
+            'data'=>$resourceData,
+            'pagination' => [
+                'total'        => $paginator->total(),
+                'count'        => $paginator->count(),
+                'per_page'     => $paginator->perPage(),
+                'current_page' => $paginator->currentPage(),
+                'total_pages'  => $paginator->lastPage(),
+            ],
+        ];
+        return response()->json($array,$status);
+    }
 }
